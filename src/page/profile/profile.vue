218ff0df911fa3b4037502ebd8cc661664187bdd
‘tianya20180’ <1320249650@qq.com>
2021-05-23 17:29:35 +0800

    fix

--- src/page/profile/profile.vue
+++ src/page/profile/profile.vue
@@ -107,16 +107,7 @@
 					    </span>
 					</div>
 				</router-link>
-				<router-link to="/articleList" class="myorder">
-					<div class="myorder-div" style="border-bottom:0;" v-if="this.identity==0">
-					    <span >我的处方单</span>
-					    <span class="myorder-divsvg">
-					        <svg fill="#bbb">
-					            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
-					        </svg>
-					    </span>
-					</div>
-				</router-link>
+			
 				<router-link to="/orderList" class="myorder">
 					<div class="myorder-div" style="border-bottom:0;" v-if="this.identity==0">
 					    <span >我的订单</span>
